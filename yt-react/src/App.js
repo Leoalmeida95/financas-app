f55fcbbe3fd78d75e6936d1fe8b762507054c65a
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-07 01:34:48 -0300

    css

--- yt-react/src/App.js
+++ yt-react/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from './views/login';
 
 import 'bootswatch/dist/sketchy/bootstrap.css';
+import './custom.css'
 
 class App extends React.Component{
 
--- yt-react/src/views/login.js
+++ yt-react/src/views/login.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import Card from '../components/card'
-class Login extends React.Component{
+import FormGroup from '../components/form-group'
 
-    render(){
-        return(
+class Login extends React.Component {
+
+    state = {
+        email: '',
+        senha: ''
+    }
+
+    entrar = () => {
+
+    }
+
+    render() {
+        return (
             <div className="container">
                 <div className="row">
-                        <div className="col-md-6" style={ {position: 'relative', left:'300px'}}>
-                            <Card title='Login'>
-                                    Meu cartão
-                            </Card>
-                        </div>
+                    <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                        <Card title='Login'>
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                <input type="email"
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control" id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp" placeholder="Digite o Email" />
+                                            </FormGroup>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputSenha1">
+                                                <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control" id="exampleInputSenha1"
+                                                    placeholder="Senha" />
+                                            </FormGroup>
+                                            <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                            <button className="btn btn-warning">Cadastrar</button>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
                 </div>
             </div>
         )
