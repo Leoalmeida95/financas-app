83417b95ee8d20ee42bd81eb33755e0412fb5eed
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-07 00:21:13 -0300

    add dependencies bootswatch

--- yt-react/src/App.js
+++ yt-react/src/App.js
@@ -10,29 +10,10 @@ import React from 'react';
 // }
 
 class App extends React.Component{
-
-  state = {
-    numero1: null,
-    numero2:null,
-    resultado:null
-  }
-
-  somar = () => {
-    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2);
-    this.setState({resultado:resultado});
-  }
-
   render(){
     return(
     <div>
-      <label>Entre com os valores:</label>
-      <input type="text" values={this.state.numero1} onChange={(e) => this.setState({numero1: e.target.value})}></input> +
-      <input type="text" values={this.state.numero2} onChange={(e) => this.setState({numero2: e.target.value})}></input> 
-      <br/>
-      <button onClick={this.somar}>Somar</button>
-      <br/>
-      <br/>
-      Resultado: {this.state.resultado}
+     
      </div>
    );
   }
