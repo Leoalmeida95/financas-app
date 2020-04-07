081b25812cfd3d610714d7131bf1ac5cb3e12007
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-06 23:32:25 -0300

    declarando uma classe

--- yt-react/src/App.js
+++ yt-react/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 
 
-function App() {
-  return (
-    <div>
-        hello world
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//         hello world
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return(
+           <div>
+         hello world
+     </div>
+   );
+  }
 }
 
 export default App;
